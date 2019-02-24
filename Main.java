bee2a6f2edb067190e5bf05831758a086e2aae65
Stefan Traistaru <stefan.traistaru.97@gmail.com>
2019-02-24 13:11:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
  
 public class Main {
 	
-	// Retin valorile variabilelor
+	// Used to keep the values of the variables
 	public static HashMap<String, Integer> values = new HashMap<>();
 	
 	public static String addNewline(String print) {
@@ -25,23 +25,23 @@ public class Main {
 		l.stack.push(e);
 		l.stack.push(new MainNode(e));
 		
-		// Scriere AST
+		// Writing AST
 		BufferedWriter writer = new BufferedWriter(new FileWriter("arbore"));
-	    writer.write(((Expression) l.stack.pop()).show());
-	    writer.close();
+	    	writer.write(((Expression) l.stack.pop()).show());
+	    	writer.close();
 	    
-	    // Initializare HashMap
-	    for (String var: l.variables) {
-	    	values.put(var, null);
-	    }
+	    	// HashMap initialization
+	    	for (String var: l.variables) {
+	    		values.put(var, null);
+	    	}
 	    
-	    // Scriere output
-	    BufferedWriter writer2 = new BufferedWriter(new FileWriter("output"));
-	    l.stack.pop().interpret();
-    	for (Map.Entry<String, Integer> pair: values.entrySet()) {
-	    	writer2.write(pair.getKey() + "=" + pair.getValue() + "\n");
-        }
-	    writer2.close();
+	    	// Writing output
+	    	BufferedWriter writer2 = new BufferedWriter(new FileWriter("output"));
+	    	l.stack.pop().interpret();
+    		for (Map.Entry<String, Integer> pair: values.entrySet()) {
+	    		writer2.write(pair.getKey() + "=" + pair.getValue() + "\n");
+        	}
+	    	writer2.close();
 	}
 	
 }
